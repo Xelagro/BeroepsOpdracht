dbb63e124271355064740f22c79edb18c3d81add
Sam <sammywels13@gmai.com>
2020-10-29 09:53:45 +0100

    BOProgress

--- ditbenik.py
+++ ditbenik.py
@@ -1,9 +1,61 @@
-naam = input("Wat is jouw naam?     ")
+print("Hello You! Ik ben Samantha!")
+print("Wie ben jij?")
 
-print ("Hallo " + naam)
+naam = input()
+print("Hoi!,", naam)
+print("Om wat meer dingen over mij te weten te komen heb ik een paar vragen, je kunt kiezen uit A, B of C.")
 
-#Dingen
-begin = "Ik heb voor u 3 vragen die u kan beantwoorden"
-vraag1 = "Waar woon ik?"
-vraag2 = "Hoe oud ben ik?"
-vraag3 = "Heb ik een broer of een zus?" 
+print("Waar woon ik")
+
+print("A: Zaandam")
+print("B: Amsterdam")
+print("C: Rotterdam")
+
+answer = input()
+
+if answer == 'A':
+	print("Fout, Ik woon wel in de buurt van Zaandam!")
+
+if answer == "B":
+	print("Goed, Ik woon in Amsterdam-Noord.")
+
+if answer == "C":
+	print("Fout, ik kom wel vaak in Rotterdam voor familie")
+
+
+print("Hoe oud ben ik?")
+
+print("A: 16 Jaar oud")
+print("B: 17 Jaar oud")
+print("C: 18 Jaar oud")
+
+answer = input()
+
+if answer == "A":
+	print("Goed!")
+
+if answer == "B":
+	print("Fout, ik ben bijna 17!")
+
+if answer == "C":
+	print("Fout, dat duurt nog 2 jaartjes.")
+
+print("In mijn vrije tijd doe ik?")
+
+print("A: Ik ga veel naar buiten")
+print("B: Ik game veel") 
+print("C: Ik slaap veel")
+
+answer = input()
+
+if answer == "A":
+	print("Fout!")
+
+if answer == "B":
+	print("Correct. Ik game best veel!")
+
+if answer == "C":
+	print("Fout!")
+
+
+print("Nu weet je wat meer dingen over mij!")
